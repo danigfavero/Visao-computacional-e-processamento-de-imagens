aa265dbd23096b4809cf43d46892a000390a68ce
danigfavero <danifavero@usp.br>
2020-09-24 16:10:33 -0300

    metadados

---
+++ generate.py
@@ -0,0 +1,14 @@
+import csv
+
+with open('metadata.csv', 'w', newline='') as file:
+    
+    writer = csv.writer(file)
+    writer.writerow(["NAME_FILE", "DATE", "CLASS", "BACKGROUND", "LIGHT", "INDOOR"])
+
+    for classe in ["COLHER", "GARFO", "CANECA", "FACA", "PANELA", "FRIGIDEIRA", "CONTROLE REMOTO", "CALCA", "CASACO", "CAMISETA"]:
+        for objeto in range(1,4):
+            for fundo in ["FUNDO1", "FUNDO2", "FUNDO3"]:
+                for light in ["DIA", "NOITE"]:
+                    for indoor in ["INDOOR", "OUTDOOR"]:
+                        for repeticao in ["a", "b", "c"]:
+                            writer.writerow([str(classe) + str(objeto) + str(repeticao), 0, classe, fundo, light, indoor])
