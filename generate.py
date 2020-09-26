b54ee0bd4a5d68d1c4b3cc17e7204f9efc5f9765
danigfavero <danifavero@usp.br>
2020-09-26 18:39:32 -0300

    arrumando metadado

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ with open('metadata.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["NAME_FILE", "DATE", "CLASS", "BACKGROUND", "LIGHT", "INDOOR"])
 
-    for classe in ["COLHER", "GARFO", "CANECA", "FACA", "PANELA", "FRIGIDEIRA", "CONTROLE REMOTO", "CALCA", "CASACO", "CAMISETA"]:
+    for classe in ["COLHER", "GARFO", "CANECA", "FACA", "PANELA", "FRIGIDEIRA", "CONTROLE", "CALCA", "CASACO", "CAMISETA"]:
         for objeto in range(1,4):
             for fundo in ["ESCURO", "CLARO", "MADEIRA"]:
                 for light in ["DIA", "NOITE"]:
